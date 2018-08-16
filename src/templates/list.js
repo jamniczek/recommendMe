16e0388b7026acd7dc9396fa658c15a4b7290eb9
Maciej Misiak <misiak.mac@gmail.com>
2018-08-16 14:22:05 +0200

    add templates

---
+++ src/templates/list.js
@@ -0,0 +1,40 @@
+class List {
+    constructor() {
+        this.payload = {
+            template_type: "list",
+            top_element_style: "COMPACT",
+            elements: []
+        };
+    }
+}
+
+class ListElement {
+    constructor(title, subtitle, imgURL) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.image_url = imgURL;
+        this.buttons = [];
+        
+    }
+}
+
+// "payload": {
+//     "template_type": "list",
+//     "top_element_style": "<LARGE | COMPACT>",
+//     "elements": [
+//       {
+//         "title": "<TITLE_TEXT>",
+//         "subtitle": "<SUBTITLE_TEXT>",
+//         "image_url": "<IMAGE_URL_FOR_THUMBNAIL>",          
+//         "buttons": [<BUTTON_OBJECT>],
+//         "default_action": {
+//           "type": "web_url",
+//           "url": "<URL_TO_OPEN_WHEN_ITEM_IS_TAPPED>",
+//           "messenger_extensions": <TRUE | FALSE>,
+//           "webview_height_ratio": "<COMPACT | TALL | FULL>"
+//         }
+//       },
+//       ...
+//     ],
+//      "buttons": [<BUTTON_OBJECT>]  
+//   
