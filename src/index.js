41cd353a26544a6bbaae0afe13d155d027f82b3b
Maciej Misiak <misiak.mac@gmail.com>
2018-08-16 14:44:17 +0200

    puhs to heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const templates = require('./templates')
 
 const keys = require('./config/keys');
 
@@ -12,6 +13,22 @@ app.use(bodyParser.json());
 // just for booting app on heroku, to avoid delay/500 on the first request
 app.get('/boot', () => {
 
+});
+
+app.post('/dialogflow/recommend', (req, res) => {
+  console.log(req.body)
+  const titles = req.body.titles.map(title => encodeURI(title));
+  const { type } = req.body;
+
+  axios.get(`https://tastedive.com/api/similar?info=1&type=${type}&limit=5&q=${titles[0]}%2C${titles[1]}&%2C${titles[2]}&k=${keys.tastediveKey}`)
+    .then((results) => {
+      const recomendations = results.data.Similar.Results;
+      if (recomendations.length === 0) {
+        return res.status(404).send({ message: 'No recommendations found!' });
+      }
+      return res.send(recomendations);
+    })
+    .catch(err => res.status(500).send(err.message));
 });
 
 app.post('/recommend', (req, res) => {
