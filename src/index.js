6de1e42c968f5a5abb83bf51636d9d6f0adac530
Maciej Misiak <misiak.mac@gmail.com>
2018-08-16 18:15:38 +0200

    try on fb

--- src/index.js
+++ src/index.js
@@ -53,7 +53,37 @@ app.post('/recommend', (req, res) => {
       if (recomendations.length === 0) {
         return res.status(404).send({ message: 'No recommendations found!' });
       }
-      return res.send(recomendations);
+      return res.send({"facebook": {"message":{
+        "attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"generic",
+            "elements":[
+               {
+                "title":"Welcome!",
+                "image_url":"https://petersfancybrownhats.com/company_image.png",
+                "subtitle":"We have the right hat for everyone.",
+                "default_action": {
+                  "type": "web_url",
+                  "url": "https://petersfancybrownhats.com/view?item=103",
+                  "webview_height_ratio": "tall",
+                },
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://petersfancybrownhats.com",
+                    "title":"View Website"
+                  },{
+                    "type":"postback",
+                    "title":"Start Chatting",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                  }              
+                ]      
+              }
+            ]
+          }
+        }
+      }}});
     })
     .catch(err => res.status(500).send(err.message));
 });
@@ -61,3 +91,35 @@ app.post('/recommend', (req, res) => {
 app.listen(PORT);
 
 module.exports = app;
+
+// {"message":{
+//   "attachment":{
+//     "type":"template",
+//     "payload":{
+//       "template_type":"generic",
+//       "elements":[
+//          {
+//           "title":"Welcome!",
+//           "image_url":"https://petersfancybrownhats.com/company_image.png",
+//           "subtitle":"We have the right hat for everyone.",
+//           "default_action": {
+//             "type": "web_url",
+//             "url": "https://petersfancybrownhats.com/view?item=103",
+//             "webview_height_ratio": "tall",
+//           },
+//           "buttons":[
+//             {
+//               "type":"web_url",
+//               "url":"https://petersfancybrownhats.com",
+//               "title":"View Website"
+//             },{
+//               "type":"postback",
+//               "title":"Start Chatting",
+//               "payload":"DEVELOPER_DEFINED_PAYLOAD"
+//             }              
+//           ]      
+//         }
+//       ]
+//     }
+//   }
+// }}
